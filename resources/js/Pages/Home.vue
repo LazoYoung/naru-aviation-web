daff3133029830605a725b90fc0ccb9921bbe619
lazoyoung <idealproduction@naver.com>
2023-06-06 23:19:06 +0900

    feature: Home page background video

---
+++ resources/js/Pages/Home.vue
@@ -0,0 +1,58 @@
+<script setup>
+import MainLayout from '@/Layouts/MainLayout.vue';
+import { Head } from '@inertiajs/vue3';
+
+defineProps({
+    canLogin: {
+        type: Boolean,
+    },
+    canRegister: {
+        type: Boolean,
+    },
+    laravelVersion: {
+        type: String,
+        required: true,
+    },
+    phpVersion: {
+        type: String,
+        required: true,
+    },
+});
+</script>
+
+<template>
+    <Head title="Home" />
+
+    <MainLayout>
+        <div class="fixed min-w-full min-h-full overflow-hidden">
+            <video autoplay loop muted disablePictureInPicture>
+                <source src="/promo.mp4">
+            </video>
+        </div>
+
+        <div class="z-10 relative top-0 h-screen invisible"></div>
+
+        <div class="z-10 relative w-full h-screen bg-black sm:px-6 lg:px-8">
+            <div class="py-80">blank area</div>
+
+            <div class="flex justify-center mt-16 px-6 sm:items-center sm:justify-between">
+                <div class="text-center text-sm text-gray-500 dark:text-gray-400 sm:text-left">
+                    <div class="flex items-center gap-4">
+                        <a
+                            href="https://github.com/LazoYoung/virtual-airline-web"
+                            target="_blank"
+                            class="group inline-flex items-center hover:text-gray-700 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500"
+                        >
+                            <i class="fa-brands fa-github"></i>
+                            <p class="ms-2">Crafted by Chanyoung Park</p>
+                        </a>
+                    </div>
+                </div>
+
+                <div class="ml-4 text-center text-sm text-gray-500 dark:text-gray-400 sm:text-right sm:ml-0">
+                    Laravel v{{ laravelVersion }} (PHP v{{ phpVersion }})
+                </div>
+            </div>
+        </div>
+    </MainLayout>
+</template>
--- routes/web.php
+++ routes/web.php
@@ -21,21 +21,17 @@ use Inertia\Inertia;
 */
 
 Route::get('/', function () {
-    return Inertia::render('Welcome', [
+    return Inertia::render('Home', [
         'canLogin' => Route::has('login'),
         'canRegister' => Route::has('register'),
         'laravelVersion' => Application::VERSION,
         'phpVersion' => PHP_VERSION,
     ]);
-});
+})->name('home');
 
 Route::get('/forum', [ForumController::class, 'getView'])
     ->middleware(['auth', 'verified'])
     ->name('forum.browse');
-
-Route::get('/dashboard', function () { return Inertia::render('Dashboard'); })
-    ->middleware(['auth', 'verified'])
-    ->name('dashboard');
 
 Route::controller(ThreadController::class)->group(function () {
     Route::get('/thread', 'show')
