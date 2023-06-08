878c7f355a2220919f7fd89939084ef557aa6075
lazoyoung <idealproduction@naver.com>
2023-06-08 13:30:21 +0900

    refactor: Discard vuetify

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,14 +1,11 @@
 import './bootstrap';
-import '../css/app.css';
+import '../css/tailwind.css';
 import '../scss/app.scss';
 
 import { createApp, h } from 'vue';
 import { createInertiaApp } from '@inertiajs/vue3';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { ZiggyVue } from '../../vendor/tightenco/ziggy/dist/vue.es.js';
-import {createVuetify} from 'vuetify';
-import * as components from 'vuetify/components';
-import * as directives from 'vuetify/directives';
 
 const appName = 'Naru Aviation';
 
@@ -16,15 +13,10 @@ createInertiaApp({
     title: () => `${appName}`,
     resolve: (name) => resolvePageComponent(`./Pages/${name}.vue`, import.meta.glob('./Pages/**/*.vue')),
     setup({ el, App, props, plugin }) {
-        const vuetify = createVuetify({
-            components,
-            directives
-        });
 
         return createApp({render: () => h(App, props)})
             .use(plugin)
             .use(ZiggyVue)
-            .use(vuetify)
             .mount(el);
     },
     progress: {
