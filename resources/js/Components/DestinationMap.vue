ed77031268965445ccaa1f59abf88011be59fe62
lazoyoung <idealproduction@naver.com>
2023-06-18 21:39:03 +0900

    feature: Map icon renewal

--- resources/js/Components/DestinationMap.vue
+++ resources/js/Components/DestinationMap.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { Loader } from "@googlemaps/js-api-loader"
+import {Loader} from "@googlemaps/js-api-loader"
 import {onMounted} from "vue";
 import {fetchJSON} from "@/api.js";
 import {usePage} from "@inertiajs/vue3";
@@ -15,15 +15,18 @@ onMounted(() => {
         const {Map} = await google.maps.importLibrary('maps');
         const map = new Map(document.getElementById('map'), {
             center: {lat: 37.0, lng: 128.644},
-            zoom: 1,
-            mapId: 'DEMO_MAP_ID',
+            zoom: 2,
+            mapId: '3969ad62e13f6478',
+            mapTypeControl: false,
+            scaleControl: false,
+            streetViewControl: false,
         });
 
-        await fetchAirports(map);
+        await renderMarkers(map);
     });
 });
 
-async function fetchAirports(map) {
+async function renderMarkers(map) {
     let data = await fetchJSON(route('airport.fetch.all'), csrfToken);
 
     if (!data) return;
@@ -34,13 +37,24 @@ async function fetchAirports(map) {
 }
 
 async function addMarker(map, airport) {
-    let {AdvancedMarkerElement} = await google.maps.importLibrary('marker');
+    let {InfoWindow} = await google.maps.importLibrary('maps');
+    let {AdvancedMarkerElement, PinElement} = await google.maps.importLibrary('marker');
+    let infoWindow = new InfoWindow();
     let latitude = parseFloat(airport.latitude);
     let longitude = parseFloat(airport.longitude);
-    const infoWindow = new google.maps.InfoWindow();
+    let icon = document.createElement('div');
+    icon.innerHTML = '<i class="fa-solid fa-plane-up fa-lg"></i>';
+    let pin = new PinElement({
+        scale: 1.2,
+        glyph: icon,
+        glyphColor: '#2836dc',
+        background: '#bebebe',
+        borderColor: '#000000',
+    });
     let marker = new AdvancedMarkerElement({
         map: map,
         position: { lat: latitude, lng: longitude },
+        content: pin.element,
         title: airport.name,
     });
     marker.addListener("click", () => {
@@ -52,5 +66,5 @@ async function addMarker(map, airport) {
 </script>
 
 <template>
-    <div id="map" class="h-full w-full"/>
+    <div id="map" class="h-full"/>
 </template>
