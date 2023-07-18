35ee4131839b96379adf12126f456af2799b1dfe
LazoYoung <idealproduction@naver.com>
2023-07-18 11:37:10 +0900

    refactor: workflow scripts

---
+++ deploy.sh
@@ -0,0 +1,7 @@
+session=github-actions-naru-aviation
+tmux has-session -t $session || tmux new-session -d -s $session
+tmux send-keys -t $session C-c
+tmux send-keys -t $session "git reset --hard" C-m
+tmux send-keys -t $session "git checkout main" C-m
+tmux send-keys -t $session "git pull" C-m
+tmux send-keys -t $session "npm run build" C-m
