ace23d4d1cbda130cc060de0bb519fd611494ced
LazoYoung <idealproduction@naver.com>
2023-07-18 18:57:52 +0900

    fix: deploy fails due to missing libraries

--- deploy.sh
+++ deploy.sh
@@ -4,4 +4,6 @@ tmux send-keys -t $session C-c
 tmux send-keys -t $session "git reset --hard" C-m
 tmux send-keys -t $session "git checkout main" C-m
 tmux send-keys -t $session "git pull" C-m
+tmux send-keys -t $session "composer install" C-m
+tmux send-keys -t $session "npm install" C-m
 tmux send-keys -t $session "npm run build" C-m
