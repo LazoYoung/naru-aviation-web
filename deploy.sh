5dc1d0fe7e27ac3d79e4f8d47fdbebc9d5c6db8b
LazoYoung <idealproduction@naver.com>
2023-07-19 13:36:35 +0900

    fix: delete redundant steps

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,9 @@
-session=github-actions-naru-aviation
+ref = $1
+session = github-actions-naru-aviation
 tmux has-session -t $session || tmux new-session -d -s $session
 tmux send-keys -t $session C-c
 tmux send-keys -t $session "git reset --hard" C-m
-tmux send-keys -t $session "git checkout main" C-m
+tmux send-keys -t $session "git checkout $ref" C-m
 tmux send-keys -t $session "git pull" C-m
 tmux send-keys -t $session "composer install" C-m
 tmux send-keys -t $session "npm install" C-m
