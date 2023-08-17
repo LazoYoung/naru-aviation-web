95148b6e0ebe7a4f98d66aaa2db005af95859fbf
LazoYoung <idealproduction@naver.com>
2023-08-17 23:04:44 +0900

    fix: redirect after sign out

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -40,14 +40,17 @@ class AuthenticatedSessionController extends Controller
     /**
      * Destroy an authenticated session.
      */
-    public function destroy(Request $request): RedirectResponse
-    {
+    public function destroy(Request $request): RedirectResponse|\Symfony\Component\HttpFoundation\Response {
         Auth::guard('web')->logout();
 
         $request->session()->invalidate();
 
         $request->session()->regenerateToken();
 
-        return redirect('/');
+        if ($request->inertia()) {
+            return Inertia::location('/');
+        } else {
+            return to_route('home');
+        }
     }
 }
