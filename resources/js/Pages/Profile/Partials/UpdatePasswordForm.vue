b976d3745b0f81f6b7db100019beb54e9c877e8c
LazoYoung <idealproduction@naver.com>
2023-08-18 15:41:23 +0900

    fix: unused imports

--- resources/js/Pages/Profile/Partials/UpdatePasswordForm.vue
+++ resources/js/Pages/Profile/Partials/UpdatePasswordForm.vue
@@ -1,10 +1,8 @@
 <script setup>
 import InputError from '@/Components/InputError.vue';
-import InputLabel from '@/Components/InputLabel.vue';
-import PrimaryButton from '@/Components/PrimaryButton.vue';
 import TextInput from '@/Components/TextInput.vue';
-import { useForm } from '@inertiajs/vue3';
-import { ref } from 'vue';
+import {useForm} from '@inertiajs/vue3';
+import {ref} from 'vue';
 import Alert from "@/alert.js";
 
 const passwordInput = ref(null);
